86867db47cc4aca155865f96401d6e63e08f2cf1
GBethani <jinkiller69@gmail.com>
2021-05-13 12:58:00 +0530

    account admin, password readonly added

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 # Register your models here.
 
-admin.site.register(Account)
+class AccountAdmin(UserAdmin):
+    list_display = ('first_name','last_name','username','email','last_login',)
+    list_filter = ('date_joined','is_staff')
+    readonly_fields = ('date_joined','last_login',)
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    fieldsets = ()  # keeps the password read only
+    
+admin.site.register(Account,AccountAdmin)
