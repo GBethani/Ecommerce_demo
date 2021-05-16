d8ce933c9b005e3efb2679eeccd5a9a2bd469b67
GBethani <jinkiller69@gmail.com>
2021-05-16 11:23:00 +0530

    categories context processors,categories to products home page added

--- products/views.py
+++ products/views.py
@@ -3,8 +3,8 @@ from . models import Product
 from category.models import Category
 # Create your views here.
 def productlist(request,category_slug=None):
-    category = None
-    products = None
+    # category = None
+    # products = None
     if category_slug != None:
         category = get_object_or_404(Category,slug=category_slug)
         products = Product.objects.all().filter(category=category,availability=True)
