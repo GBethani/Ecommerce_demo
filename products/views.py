552b71eddbfae6add35469cad818d3cfd7ada458
GBethani <jinkiller69@gmail.com>
2021-05-16 10:20:42 +0530

    categories to products home page added

--- products/views.py
+++ products/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from . models import Product
+from category.models import Category
 # Create your views here.
 def productlist(request):
     products = Product.objects.all().filter(availability=True)
     product_count = products.count()
+    categories = Category.objects.all()
     context = {
         'products': products,
-        'count': product_count
+        'count': product_count,
+        'categories': categories
     }
     return render(request,'products/product-list.html',context=context)
